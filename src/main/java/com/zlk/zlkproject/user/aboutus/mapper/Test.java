42a840351d8aff2f1951768802d9a53ba4e9267e
pangxie <happy@outlook.com>
2019-11-18 10:24:04 +0800

    二组初始框架

---
+++ src/main/java/com/zlk/zlkproject/user/aboutus/mapper/Test.java
@@ -0,0 +1,10 @@
+package com.zlk.zlkproject.user.aboutus.mapper;
+
+/**
+ * @program: Test
+ * @description: 占位用文件
+ * @author: xty
+ * @create: 2019/11/18 10:20
+ **/
+public class Test {
+}
---
+++ src/main/java/com/zlk/zlkproject/user/signup/mapper/Test.java
@@ -0,0 +1,10 @@
+package com.zlk.zlkproject.user.signup.mapper;
+
+/**
+ * @program: Test
+ * @description: 占位用文件
+ * @author: xty
+ * @create: 2019/11/18 10:22
+ **/
+public class Test {
+}
