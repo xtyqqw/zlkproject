d95bc2af7a6269ce4ef54d3a8abe37b93d74fa7f
123 <709329612@qq.com>
2019-11-22 17:30:46 +0800

    添加瀑布流获取数据

--- src/main/java/com/zlk/zlkproject/course/courseHomePage/service/CourseHomePageService.java
+++ src/main/java/com/zlk/zlkproject/course/courseHomePage/service/CourseHomePageService.java
@@ -2,8 +2,30 @@ package com.zlk.zlkproject.course.courseHomePage.service;
 
 
 import com.zlk.zlkproject.entity.Courses;
+import com.zlk.zlkproject.entity.Pagination;
+
+import java.util.List;
 
 
 public interface CourseHomePageService {
+    /**
+     *@Author luanke
+     *@Description //根据Id查询
+     *@Date 19:25 2019/11/21
+     *@Param [coursesId]
+     *@return com.zlk.zlkproject.entity.Courses
+    **/
     Courses selectCoursesByCoursesId(Integer coursesId);
+
+
+    /**
+     *@Author luanke
+     *@Description //查询所有
+     *@Date 19:25 2019/11/21
+     *@Param [pagination]
+     *@return java.util.List<com.zlk.zlkproject.entity.Courses>
+    **/
+    List<Courses> findCoursesList(Pagination pagination);
+
+    Integer findCoursesCount(Pagination pagination);
 }
--- src/main/java/com/zlk/zlkproject/course/courseHomePage/service/impl/CourseHomePageServiceImpl.java
+++ src/main/java/com/zlk/zlkproject/course/courseHomePage/service/impl/CourseHomePageServiceImpl.java
@@ -4,8 +4,11 @@ import com.zlk.zlkproject.course.courseHomePage.mapper.CourseHomePageMapper;
 import com.zlk.zlkproject.course.courseHomePage.service.CourseHomePageService;
 
 import com.zlk.zlkproject.entity.Courses;
+import com.zlk.zlkproject.entity.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @ClassName CourseHomePageServiceImpl
@@ -16,11 +19,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class CourseHomePageServiceImpl implements CourseHomePageService {
 
-    @Autowired
+    @Autowired(required = false)
     private CourseHomePageMapper courseHomePageMapper;
 
     @Override
     public Courses selectCoursesByCoursesId(Integer coursesId) {
         return courseHomePageMapper.selectCoursesByCoursesId(coursesId);
+    }
+
+    @Override
+    public List<Courses> findCoursesList(Pagination pagination) {
+        Integer page = pagination.getPage();
+        Integer limit = pagination.getLimit();
+        Integer startPage = (page-1)*limit;
+        pagination.setStartPage(startPage);
+        return courseHomePageMapper.findCoursesList(pagination);
+    }
+
+    @Override
+    public Integer findCoursesCount(Pagination pagination) {
+        return courseHomePageMapper.findCoursesCount(pagination);
     }
 }
