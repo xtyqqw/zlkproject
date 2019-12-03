19ee4e33184425eec7c2d9f98d23330e6b4750ff
e <3160790985@qq.com>
2019-12-03 17:36:53 +0800

    笔记管理的修改方法

--- src/main/java/com/zlk/zlkproject/course/stuNoteAdmin/controller/StuNoteAdminController.java
+++ src/main/java/com/zlk/zlkproject/course/stuNoteAdmin/controller/StuNoteAdminController.java
@@ -1,11 +1,14 @@
 package com.zlk.zlkproject.course.stuNoteAdmin.controller;
 
+import com.zlk.zlkproject.admin.util.LogUtil;
 import com.zlk.zlkproject.course.stuNoteAdmin.service.StuNoteAdminService;
+import com.zlk.zlkproject.entity.StuNote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +19,10 @@ import java.util.Map;
  */
 @Controller
 public class StuNoteAdminController {
+
+    @Autowired
+    private LogUtil logUtil;
+
     @Autowired
     private StuNoteAdminService stuNoteAdminService;
 
@@ -37,15 +44,33 @@ public class StuNoteAdminController {
     }
 
     /**
+     * 根据笔记对象修改笔记
+     * @param stuNote
+     * @param request
+     * @return
+     */
+    @RequestMapping("/stuNote/updateStuNoteByStuNote")
+    @ResponseBody
+    public String updateStuNoteByStuNote(StuNote stuNote, HttpServletRequest request){
+        int i = stuNoteAdminService.updateStuNoteByStuNote(stuNote);
+        if(i>0){
+            logUtil.setLog(request,"修改了笔记内容为"+stuNote+"的信息");
+            return "修改成功";
+        }
+        return "修改失败";
+    }
+
+    /**
      * 根据id删除数据
      * @param snId
      * @return
      */
     @RequestMapping("/stuNote/deleteStuNoteBySnId")
     @ResponseBody
-    public String deleteStuNoteBySnId(Integer snId){
+    public String deleteStuNoteBySnId(Integer snId, HttpServletRequest request){
         int tiaoshu = stuNoteAdminService.deleteStuNoteBySnId(snId);
         if (tiaoshu>0){
+            logUtil.setLog(request,"删除了笔记id为"+snId+"的信息");
             return "删除成功";
         }
         return "删除失败";
