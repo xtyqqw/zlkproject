8d534020a3ccf38b9e920adb7c0dbcc765052ad5
kaiguan <byu690504@163.com>
2019-12-06 19:46:54 +0800

    配置文件中添加JPA

--- src/main/java/com/zlk/zlkproject/community/articleShow/controller/ArticleShowController.java
+++ src/main/java/com/zlk/zlkproject/community/articleShow/controller/ArticleShowController.java
@@ -25,16 +25,7 @@ public class ArticleShowController {
     @GetMapping(value = "/community/article-show")
     public ModelAndView articleShow(String articleId) {
         ModelAndView mv=new ModelAndView();
-        articleId="492352066";
-        mv.addObject("article", articleShowService.getAndConvert(articleId));
-        mv.setViewName("view/community/articleShow");
-        return mv;
-    }
-
-    //跳转文章详情页测试用方法，使用时解开注释
-    @GetMapping(value = "/community/article-show/{articleId}")
-    public ModelAndView articleShowTest(String articleId) {
-        ModelAndView mv=new ModelAndView();
+        articleId="1984163747";
         mv.addObject("article", articleShowService.getAndConvert(articleId));
         mv.setViewName("view/community/articleShow");
         return mv;
