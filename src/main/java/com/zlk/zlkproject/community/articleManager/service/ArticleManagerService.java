405d86c08a5287b7452d0a851e8d18a72abe9838
zhaowei1693974437 <zhaowei1693974437@163.com>
2019-12-11 09:10:05 +0800

    测试150

--- src/main/java/com/zlk/zlkproject/community/articleManager/service/ArticleManagerService.java
+++ src/main/java/com/zlk/zlkproject/community/articleManager/service/ArticleManagerService.java
@@ -2,6 +2,7 @@ package com.zlk.zlkproject.community.articleManager.service;
 
 import com.zlk.zlkproject.admin.util.Pagination;
 import com.zlk.zlkproject.entity.Article;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -24,6 +25,12 @@ public interface ArticleManagerService {
      * @return
      */
     Integer deleteArticleByArticleId(String articleId);
+    /**
+     * 根据文章id批量删除文章
+     * @param articleList
+     * @return
+     */
+    Integer deleteArticlesByArticleId(List<Article> articleList);
     /**
      * 根据文章id修改文章内容
      * @param article
