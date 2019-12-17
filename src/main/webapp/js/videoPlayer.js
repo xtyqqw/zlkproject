e4a89887baf26cb7b60604897056fa58010dff80
zhang <3230256690@qq.com>
2019-12-17 14:12:47 +0800

    修改bug

--- src/main/webapp/js/videoPlayer.js
+++ src/main/webapp/js/videoPlayer.js
@@ -228,7 +228,9 @@ $(document).ready(function () {
                             }*/
                         }
                     });
-                    stu_qa_flow("#stuQaall",basePath+"/stuQa/findStuQaList",sectionId);
+                    // stu_qa_flow("#stuQaall",basePath+"/stuQa/findStuQaList",sectionId);
+                }else {
+                    layer.msg("请确认字数未超过限制");
                 }
             }
         });
@@ -300,7 +302,9 @@ $(document).ready(function () {
         $("#text_div").keyup(function () {
             if (checkLength(editor,401)){
                 layer.msg("输入内容请不要超过200个汉字或400个英文字符");
-                istrue = false;
+                isSubmit = false;
+            }else {
+                isSubmit = true;
             }
         });
 
