07c752108fdd2d5a4eb18d6bf82e33e84b60321d
kaiguan <byu690504@163.com>
2019-12-24 18:03:30 +0800

    评论完善

--- src/main/java/com/zlk/zlkproject/entity/User.java
+++ src/main/java/com/zlk/zlkproject/entity/User.java
@@ -65,10 +65,12 @@ public class User {
     private String userSpecialty;
     /**用户总学习时间(小时数)*/
     private Integer userAllTime;
+    /*使用该注解不会在数据库中创建该字段*/
     @Transient
     private Double userAllTimeDou;
     /**用户每日学习时长*/
     private Integer userDateTime;
+    /*使用该注解不会在数据库中创建该字段*/
     @Transient
     private Double userDateTimeDou;
     /**头像图片相对路径*/
